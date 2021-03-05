84e6948f80f9dbe9f005bfd82e125910481c9172
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-03-05 12:28:42 -0800

    Learning Redux

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? (
+        <h4>super secret info</h4>
+      ) : (
+        <h4>no secret info for youuuu</h4>
+      )}
+    </div>
+  );
+}
+
+export default App;
